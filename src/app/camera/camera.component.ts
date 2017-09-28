e684cd100e67d903bbef69d161ee166c77cb04dd
Brian McGowan <mcgowan.b@gmail.com>
2017-09-28 18:25:15 +0100

    css added

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./camera.component.css']
 })
 export class CameraComponent implements OnInit {
-
+  background = "../assets/images/background/sligo.jpg";
   constructor() { }
 
   ngOnInit() {
