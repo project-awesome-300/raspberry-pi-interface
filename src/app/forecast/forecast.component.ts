ef4c6fc7a6786e9dbe51ced9825ffc72b9dcabaa
kateoneill <S00165383@mail.itsligo.ie>
2017-10-18 17:19:40 +0100

    Basic forecast

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Forecast } from '../../models/forecast';
+import { WeatherService } from '../../providers/weather.service';
 
 @Component({
   selector: 'app-forecast',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forecast.component.css']
 })
 export class ForecastComponent implements OnInit {
+  myForecast: Forecast[] = [];
 
-  constructor() { }
+  constructor(private ws:WeatherService) { }
 
   ngOnInit() {
+   // this.myForecast =this.ws.forecastNow();
+    const lat = 54.2697;
+    const lon = -8.4695;
+ this.ws.forecastWeather(lat, lon).subscribe(
+      (data) =>{
+        console.log(data);
+        for(let i=0; i<data.list.length;i= i+8){
+          const forecastWeather = new Forecast(data.city.name,
+                                                data.list[i].main.temp_min,
+                                                data.list[i].main.temp_min,
+                                                data.list[i].dt_txt,
+                                                data.list[i].weather[0].icon);
+          console.log(forecastWeather);
+          this.myForecast.push(forecastWeather);
+        }
+        console.log(this.myForecast);
+        return this.myForecast;
+      } 
+                                      
+    )
+  }
+    
   }
 
-}
--- src/providers/weather.service.ts
+++ src/providers/weather.service.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@angular/core';
 import { CurrentWeather } from '../models/current-weather';
 import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
+import { Forecast } from '../models/forecast';
+import * as moment from 'moment';
 
 @Injectable()
 export class WeatherService {
    current:CurrentWeather = new CurrentWeather('New York', '80', 'https://png.icons8.com/sun/color/24','sunny', '96','72')
+   dummyForecast:Forecast = new Forecast('Sligo','95','80','12/12/12','https://png.icons8.com/sun/color/24')
    //current:CurrentWeather;
     constructor(private http:Http) {}
 
@@ -13,8 +16,32 @@ export class WeatherService {
         return this.current;
     } 
 
+    forecastNow()
+{
+    return this.dummyForecast;
+}
     localWeather(lat:number, lon:number){
         return this.http.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f9910e16c173698955d13abe28b6879a&units=metric`)
         .map((response:Response) => response.json());
     }
-}
+
+    forecastWeather(lat:number, lon:number){
+        return this.http.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=f9910e16c173698955d13abe28b6879a&units=metric`)
+        .map((response:Response) => response.json());
+    }
+
+
+    groupWeatherByDay(list) {
+        const days = new Map() // use Map as need we to maintain insertion order
+      
+        list.forEach( (w) => {
+          const day = moment(w.dt*1000).format("dddd Do MMMM")
+          if( !days[day] ) days[day] = []
+          days[day].push(w)
+        })
+      
+        return days;
+      }
+      
+      
+    }
