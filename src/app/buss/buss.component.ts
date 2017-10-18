241543f349c5424355738a8f06f7e0d97ca01044
Unknown <rubabramzan96@gmail.com>
2017-10-18 14:35:11 +0100

    trying to get api working

--- src/app/buss/buss.component.ts
+++ src/app/buss/buss.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BussService } from './buss.service'; 
+import { Http,Response } from '@angular/http'; 
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-buss',
@@ -7,11 +9,37 @@ import { BussService } from './buss.service';
   styleUrls: ['./buss.component.css']
 })
 export class BussComponent implements OnInit {
+title='app works';
+private ApiUrl = 'https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=7602&format=json';
+
+data: any= {};
+
+constructor(private http:Http){
+  console.log('wotking....')
+
+  this.getData();
+  this.getBussTimetbles();
+
+}
+//getting api data
+getData(){
+  return this.http.get(this.ApiUrl)
+  .map((res:Response)=> res.json)
+}
+
+getBussTimetbles(){
+  this.getData().subscribe(data => {
+    console.log(data)
+    this.data=data;
+  })
+}
 
 
+
+/*
   constructor(private _busService: BussService){
     
-   } 
+   } */
 
   ngOnInit() {
   }
