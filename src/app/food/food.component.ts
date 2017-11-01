e4582add7122bb904b8b6af337460e6015bc3a36
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2017-11-01 15:10:15 +0000

    a

--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -64,6 +64,46 @@ export class FoodComponent implements OnInit {
     });
   }
 
+  onMapLoad(map){
+    console.log(map);
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      this.zoom = 12;
+   
+
+
+    console.log({ lat: this.latitude, lng:this.longitude });
+    const service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: { lat: this.latitude, lng:this.longitude },
+        radius: 5000,
+        types: ['restaurant']
+      }, function (results, status) {
+        console.log(results);
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            // createMarker(results[i]);
+            const place = results[i];
+            var placeLoc = place.geometry.location;
+            var marker = new google.maps.Marker({
+               map: map,
+              position: place.geometry.location
+            });
+
+            // google.maps.event.addListener(marker, 'click', function() {
+            //  infowindow.setContent(place.name);
+            //  infowindow.open(map, this);
+            // });
+          }
+        }
+      });
+
+    });
+    
+  }
+
   private setCurrentPosition() {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
