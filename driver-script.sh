03ea61d9547981361060bfaf3640fd809a8d4d22
Brian McGowan <mcgowan.b@gmail.com>
2017-11-28 21:24:36 +0000

    adding files from pi

--- driver-script.sh
+++ driver-script.sh
@@ -3,6 +3,4 @@ cd /home/pi/test && ng serve &
 sleep 90
 #startx &
 #sleep 10
-firefox  -P "web" -no-remote http://localhost:4200 &
-sleep 5
-xdotool key F11 
+firefox  -P "web" -no-remote http://localhost:4200 & 
