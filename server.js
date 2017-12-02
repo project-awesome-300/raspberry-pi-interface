922650f38708a2c5a9325129fe4d3d33588a8899
Raspberry PI <projectawesome1718@gmail.com>
2017-12-02 22:23:12 +0000

    adding port output to console

--- server.js
+++ server.js
@@ -18,3 +18,4 @@ app.get('/*', function(req, res) {
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8000);
+console.log('server running on port ' + (process.env.PORT || 8000))
