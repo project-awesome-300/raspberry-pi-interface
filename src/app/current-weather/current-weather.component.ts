723497f029622eb56944ca2352299e11a973684e
Brian McGowan <mcgowan.b@gmail.com>
2017-12-18 22:46:05 +0000

    code cleanup

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -9,23 +9,24 @@ import { CurrentWeather } from '../../models/current-weather';
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
-  myWeather:CurrentWeather;
-  
-  constructor(private ws:WeatherService) { }
+  myWeather: CurrentWeather;
+
+  constructor(private ws: WeatherService) { }
 
   ngOnInit() {
     this.myWeather = this.ws.weatherNow();
     const lat = 54.2697;
     const lon = -8.4695;
     this.ws.localWeather(lat, lon).subscribe(
-      (data) =>{
-        console.log(data);
-        this.myWeather = new CurrentWeather(data.name,
-                                            data.main.temp,
-                                            data.weather[0].icon,
-                                            data.weather[0].description,
-                                            data.main.temp_max,
-                                            data.main.temp_min)
+      (data) => { 
+        this.myWeather = new CurrentWeather(
+          data.name, 
+          data.main.temp, 
+          data.weather[0].icon, 
+          data.weather[0].description, 
+          data.main.temp_max, 
+          data.main.temp_min
+        );
       }
     )
   }
--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -20,141 +20,30 @@ export class ForecastComponent implements OnInit {
   constructor(private ws: WeatherService, private _app: AppService) { }
 
   ngOnInit() {
-    // this.myForecast =this.ws.forecastNow();
-    this.ws.forecastWeather(this._app.lat, this._app.lng).subscribe(
-      (data) => {
-        console.log('this is data');
-        console.log(data);
+    this.ws.forecastWeather(this._app.lat, this._app.lng).subscribe((data) => {
 
-        for (let i = 0; i < data.list.length; i+=1) {
-          /*           const forecastWeather = new Forecast(data.city.name,
-                      data.list[i].main.temp_max,
-                      data.list[i].main.temp_min,
-                      data.list[i].dt_txt,
-                      data.list[i].weather[0].icon); */
-          // const forecastWeather = {
-          //   cityName: data.city.name,
-          //   maxTemp: data.list[i].main.temp_max,
-          //   minTemp: data.list[i].main.temp_min,
-          //   date: data.list[i].dt_txt,
-          //   icon: data.list[i].weather[0].icon
-          // };
-          const forecastWeather = {
-            cityName: data.city.name,
-            maxTemp: data.list[i].main.temp_max,
-            minTemp: data.list[i].main.temp_min,
-            date: data.list[i].dt_txt,
-            icon: data.list[i].weather[0].icon,
-            desc: data.list[i].weather[0].description,
-            wind: data.list[i].wind.speed,
-            clouds: data.list[i].clouds.all
-          };
-          console.log('this is forecast weather');
-          console.log(forecastWeather);
-          this.myForecast.push(forecastWeather);
+      for (let i = 0; i < data.list.length; i += 1) {
+        const forecastWeather = {
+          cityName: data.city.name,
+          maxTemp: data.list[i].main.temp_max,
+          minTemp: data.list[i].main.temp_min,
+          date: data.list[i].dt_txt,
+          icon: data.list[i].weather[0].icon,
+          desc: data.list[i].weather[0].description,
+          wind: data.list[i].wind.speed,
+          clouds: data.list[i].clouds.all
+        };
+        this.myForecast.push(forecastWeather);
 
-
-          //working part ==============
-          this.fullDays = _.groupBy(this.myForecast, function (row) {
-            // return row.date;
-          
-            return moment(row.date).format("DDMMYYYY")
-           
-
-          }
-        
-      )
-      console.log("this.fullDays")
-      console.log(this.fullDays)
-      _.orderBy('date','asc');
-      console.log(this.fullDays)
-//=============================
-
-          //           this.fullDays = _.chain(_.map(this.days, "days"))
-          //           //.flattenDeep()
-          //           .groupBy(function (myForecast) {
-          //             console.log('data');
-          //             //console.log(this.myForecast)
-          //             console.log(data);
-          //             return moment(myForecast.date, "YYYY-MM-DD HH:MM:SS").format("DD/MM/YYYY")
-          //           })
-          //           .toPairs()
-          //           .map(function (currentItem) {
-          //             return _.zipObject(["days", this.myForecast], currentItem)
-          //           })
-          //           .value();
-          //         }
-
-          //         this.days = _.values(data)
-
-
-
-          //       console.log('days');
-          //       console.log(this.days);
-          //         console.log('this is my forecat');
-          //         console.log(this.myForecast);
-          //         console.log('^^ myforecast');
-          // /*              //this.myForecast[i] = forecastWeather;
-          //             this.days = _.chain(this.myForecast) 
-          //             .map()
-          //             .flattenDeep()
-          //             .value()
-          //             console.log('days');
-          //             console.log(this.days); */
-          //         // for(let i=0; this.myForecast.length;i++){
-
-          //         this.fullDays = _.chain(_.map(this.days, "days"))
-          //           .flattenDeep()
-          //           .groupBy(function (myForecast) {
-          //             console.log('data');
-          //             //console.log(this.myForecast)
-          //             console.log(data);
-          //             return moment(myForecast.date, "YYYY-MM-DD HH:MM:SS").format("DD/MM/YYYY")
-          //           })
-          //           .toPairs()
-          //           .map(function (currentItem) {
-          //             return _.zipObject(["date", "days"], currentItem)
-          //           })
-          //           .value();
-          //         // }
-          //         console.log('this is result');
-          //         console.log(this.fullDays);
-          //       // return this.myForecast;
-
-
-
-
-
-
-          //       })
-          //   }
-
-
-
-
-
-          /* groupForecasts(){
-          this.fullDay = _.chain(_.map(this.myForecast,"forecasts"))
-          .groupBy(function (data){
-            return moment(data.date).format("YYYY-MM-DD HH:MM:SS");
-          })
-          .toPairs()
-          .map(function (currentItem) {
-            return _.zipObject(["date", "forecasts"], currentItem);
-          })
-          .value()
-          console.log(this.myForecast);
-          } */
-
-        }
-        console.log('grouped');
-        console.log(this.fullDays);
-        	
-
-      })
+        this.fullDays = _.groupBy(this.myForecast, function (row) {
+          return moment(row.date).format("DDMMYYYY")
+        })
+        // _.orderBy('date', 'asc');
+      }
+    })
   }
-  generateArray(obj){
-    return Object.keys(obj).map((key)=>{ return obj[key]});
- }
+  generateArray(obj) {
+    return Object.keys(obj).map((key) => { return obj[key] });
+  }
 
 }
