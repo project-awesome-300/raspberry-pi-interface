c8634d8bd267b121ee0c94575c61589a40f034e2
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-03-01 13:10:33 +0000

    group by type

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,43 @@
+//services
+import { HttpService } from '../providers/http.service';
+import { CameraService } from '../providers/camera.service';
+import { WeatherService } from '../providers/weather.service';
+import { GoogleAnalyticsEventsService } from '../providers/google-analytics-events.service';
+import { GoogleMapsAPIWrapper } from '@agm/core';
+import { AppService } from '../providers/app.service';
 import { BussService } from '../providers/buss.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { WebCamComponent } from 'ack-angular-webcam/webcam.component';
-import { Routes, RouterModule } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
+//modules
+import { BootstrapModalModule } from 'ng2-bootstrap-modal';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { CameraComponent } from './camera/camera.component';
-import { FoodComponent } from './food/food.component';
-import { AgmCoreModule } from '@agm/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MapComponent } from './map/map.component';
-import { StarRatingComponent } from './star-rating/star-rating.component';
-import { CurrentWeatherComponent } from './current-weather/current-weather.component';
-import { ForecastComponent } from './forecast/forecast.component';
-import { CapitalizePipe } from '../pipes/capitalise.pipe';
-import { GroupByPipe } from '../pipes/group-by.pipe';
-import { BootstrapModalModule } from 'ng2-bootstrap-modal';
-import { ConfirmEmailComponent } from './modals/confirm-email/confirm-email.component';
-import { AppService } from '../providers/app.service';
-import { GenericModalComponent } from './modals/generic-modal/generic-modal.component';
-import { GoogleMapsAPIWrapper } from '@agm/core';
-import { GoogleAnalyticsEventsService } from '../providers/google-analytics-events.service';
-import { BussComponent } from './buss/buss.component';
-import { SundayBussComponent } from './sunday-buss/sunday-buss.component';
-import { WeatherService } from '../providers/weather.service';
-import { CameraService } from '../providers/camera.service';
-import { TranslatePipe } from '../pipes/translate.pipe';
-import { MomentPipe } from '../pipes/moment.pipe';
-import { LanguageComponent } from './language/language.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//components
 import { LocalEventsComponent } from './local-events/local-events.component';
-import { HttpService } from '../providers/http.service';
+import { LanguageComponent } from './language/language.component';
+import { SundayBussComponent } from './sunday-buss/sunday-buss.component';
+import { BussComponent } from './buss/buss.component';
+import { GenericModalComponent } from './modals/generic-modal/generic-modal.component';
+import { ConfirmEmailComponent } from './modals/confirm-email/confirm-email.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { StarRatingComponent } from './star-rating/star-rating.component';
+import { MapComponent } from './map/map.component';
+import { FoodComponent } from './food/food.component';
+import { CameraComponent } from './camera/camera.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppComponent } from './app.component';
+import { WebCamComponent } from 'ack-angular-webcam/webcam.component';
+//pipes
+import { MomentPipe } from '../pipes/moment.pipe';
+import { TranslatePipe } from '../pipes/translate.pipe';
+import { GroupByPipe } from '../pipes/group-by.pipe';
+import { CapitalizePipe } from '../pipes/capitalise.pipe';
 
-
+//routes defined
 const routes: Routes = [
   { path: '', redirectTo: 'language', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
